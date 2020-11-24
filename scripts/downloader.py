3b4dcacc3e35223986b4613752f02c952c3612c7
Kevin Coxe <kevcoxe@gmail.com>
2020-11-23 22:07:18 -0500

    remove unused code

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -9,30 +9,6 @@ from scripts.utils import clean_filename
 DOWNLOAD_OUTPUT_PATH = 'downloads'
 
 
-def download_url(yt_url):
-    try:
-        print(f'Attempting to download: {yt_url}')
-
-        yt = YouTube(yt_url)
-
-        yt.register_on_complete_callback(threaded_upload)
-        title = yt.title
-
-        download_path = yt.streams.filter(
-            progressive=True,
-            file_extension='mp4'
-            ).order_by('resolution')[-1].download(
-                output_path=DOWNLOAD_OUTPUT_PATH
-            )
-
-        print(f'Successfully downloaded: {title} to {download_path}')
-        return True, title
-
-    except Exception as e:
-        print(f'Failed to download url because: {e}')
-        return False, e
-
-
 def finish_callback(stream, filepath):
     print(f'Finished downloading: {filepath}')
 
